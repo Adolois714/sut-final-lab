9c020eaf46fa4d3541b56e084239cb39cdb63f62
Adolois714 <keemeiei@gmail.com>
2026-01-07 15:28:38 +0700

    ตรวจสอบกรณี positve close#1

---
+++ entity/employees.go
@@ -0,0 +1,19 @@
+package entity
+
+import(
+	"gorm.io/gorm"
+	"github.com/asaskevich/govalidator"
+)
+
+type Employees struct { 
+	gorm.Model 
+	Name         string   `valid:"stringlength(2|80)~Name must be 2-80 lettes"`// ชื่อต้องยาว 2-80 ตัวอักษร 
+	Salary       float64  `valid:"range(15000|200000)~Salary must be between 15000 and 200000"`// เงินเดือนต้องอยู่ในช่วง 15000-200000 
+	EmployeeCode string   `valid:"stringlength(1|7)~EmployeeCode must be 2 uppercase English letters (A-Z) followed by - and 4 digits (0-9)"`// รหัสพนักงานต้องเป็นอักษรอังกฤษตัวใหญ่ 2 ตัว ตามด้วย "-" และตัวเลข 4 ตัว (เช่น "HR-1024") 
+	}
+
+func (e Employees) Validate()(bool , error){
+	 ok ,err := govalidator.ValidateStruct(e)
+
+	return ok ,err
+}
